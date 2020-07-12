d62c9e44c1d410115f735e08d945907f7c86fc58
Wojciech Kos <obohp@wp.pl>
2020-07-12 19:51:31 +0200

    checking if createAccount return null

--- src/pl/wkos/homework111/BankAccountUtils.java
+++ src/pl/wkos/homework111/BankAccountUtils.java
@@ -8,9 +8,9 @@ public class BankAccountUtils {
             System.out.println(bankAccount.toString());
             return  bankAccount;
         } catch (NullPointerException e) {
-            System.out.println("Tworzysz konto bez osoby");
+            System.out.println("Tworzysz konto bez osoby. Konto nie zostało utworzone");
         } catch (IllegalArgumentException e) {
-            System.out.println("nie możesz tworzyć kont z ujemnym saldem");
+            System.out.println("nie możesz tworzyć kont z ujemnym saldem. Konto nie zostało utworzone");
         }
         return  null;
     }
--- src/pl/wkos/homework111/CashMachine.java
+++ src/pl/wkos/homework111/CashMachine.java
@@ -7,19 +7,25 @@ public class CashMachine {
         people[1] = new Person("Ewa", "Babacka");
 
         BankAccount[] bankAccounts = new BankAccount[3];
+
         bankAccounts[0] = BankAccountUtils.createAccount(people[0], 100);
-        bankAccounts[1] = BankAccountUtils.createAccount(people[1], -100);
-        bankAccounts[2] = BankAccountUtils.createAccount(people[2], 100);
-        try {
-            bankAccounts[0].withdraw(100);
-            bankAccounts[0].withdraw(100);
+        if (bankAccounts[0] != null) {
+            try {
+                bankAccounts[0].withdraw(100);
+                bankAccounts[0].withdraw(100);
+                System.out.println(bankAccounts[0].toString());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Nie masz tyle środków na koncie");
+            }
+            bankAccounts[0].deposit(200);
             System.out.println(bankAccounts[0].toString());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Nie masz tyle środków na koncie");
-        }
-        bankAccounts[0].deposit(200);
-        System.out.println(bankAccounts[0].toString());
-        bankAccounts[1] = BankAccountUtils.createAccount(people[1], 100_000);
+        } else
+            System.out.println("Nie ma takiego konta");
+
+        bankAccounts[1] = BankAccountUtils.createAccount(people[1], -100);
+
+        bankAccounts[2] = BankAccountUtils.createAccount(people[2], 100);
+
         bankAccounts[2] = BankAccountUtils.createAccount(new Person("Ala", "Cabacka"), 100_000);
     }
 }
